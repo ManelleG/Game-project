f9520fe10cf96ec818674fa0c9a0a3d6ab15be5d
Manelle_G;` <manelle.gueriouz@gmail.com>
2018-06-15 15:50:40 +0200

    added instructions, life bars, punching sounds and presentation

--- js/interactions.js
+++ js/interactions.js
@@ -25,6 +25,7 @@ Computer.prototype = Object.create(Character.prototype);
 
 var computer;
 var player;
+$(".life-bar").hide();
 
 //Fonction Start new game
 function startNewGame() {
@@ -32,8 +33,12 @@ function startNewGame() {
     player = new Player (100, 2);
     $("#5").addClass("computer");
     $("#1").addClass("player");
-
+    $(".player-life-bar").width(player.health);
+    $(".computer-life-bar").width(computer.health);
+    $(".player-life-bar-background").width(computer.health);
+    $(".computer-life-bar-background").width(computer.health);
 }
+
 
 //Move Left
 Character.prototype.moveLeft = function () {
@@ -76,7 +81,7 @@ Character.prototype.attack = function () {
         var that = this;
         $("#" + this.position).addClass("attack-player");
         var resetPosition = setTimeout(function () {
-            $("#" + that.position).removeClass("attack-player");
+        $("#" + that.position).removeClass("attack-player");
         }, 150);
     }
     else{
@@ -93,18 +98,36 @@ Character.prototype.attack = function () {
                 //PLAYER WON!
                 $(".board").addClass("player-won");
                 $(".board").children().remove();
+                //sound off
+                $("audio").remove()
             }
             console.log("computer.health " + computer.health)
+            $("#" + computer.position).addClass("computer-receive");
+            var resetPosition = setTimeout(function () {
+                $("#" + computer.position).removeClass("computer-receive");
+            }, 150);
+            var audio = document.querySelector("audio")
+            audio.play();
+            //Life bar reduction after each damage
+            $(".computer-life-bar").width(computer.health);
         }
         else { // if computer is attacking
             if (player.receiveDamage()){
                 //COMPUTER WON!
                 $(".board").addClass("computer-won");
                 $(".board").children().remove();
-
+                //sound off
+                $("audio").remove()
             }
-
             console.log("player.health " + player.health);
+            $("#" + player.position).addClass("player-receive");
+            var resetPosition = setTimeout(function () {
+                $("#" + player.position).removeClass("player-receive");
+            }, 150);
+            var audio = document.querySelector("audio")
+            audio.play();
+            //Life bar reduction after each damage
+            $(".player-life-bar").width(player.health);
         }
     }
     $("#" + this.position).addClass(this.type);
@@ -116,7 +139,7 @@ Character.prototype.receiveDamage = function () {
     this.health -= 5;
     if (this.health === 0){
             return true;
-    }
+        }
     return false;
 }
 
@@ -136,11 +159,12 @@ Player.prototype.mouvement = function() {
                 break;
             //Space bar
             case 32:
-                this.attack();
+            this.attack();
                 break;
         }
     }
 }
+
 
 Computer.prototype.autoMouvement = function () {
     setInterval(() => {
@@ -154,29 +178,45 @@ Computer.prototype.autoMouvement = function () {
         if (randomFunction === 2){
             this.attack();
         }
-    }, Math.floor(Math.random()*600));
+    }, (Math.floor(Math.random()*1000)) + 500);
 }
-
-
 
 
 $(document).ready(function() {
     $(".btn-start").click(function () {
         $(this).remove();
         $(".board").addClass("game-on");
+        $(".life-bar").show();
         startNewGame();
         player.mouvement();
         // "AI":
         computer.autoMouvement();
 
 
-        // var timer = 20;
-        // $("p").text(timer);
-        // var countDown = setInterval(function() {
-        //     if (timer != 0){
-        //         timer -= 1;
-        //         $("p").text(timer);
-        //     }
-        // },1000);
+        var timer = 20;
+        $("#timer").text(timer);
+        var countDown = setInterval(function() {
+            if (timer != 0){
+                timer -= 1;
+                $("#timer").text(timer);
+            }
+            else {
+                if (computer.health < player.health){
+                    //Ajouter image du player en train de gagner
+                    $(".board").addClass("player-won");
+                    $(".board").children().remove();
+                    //sound off
+                    $("audio").remove()
+                }
+                else {
+                    //Ajouter image du computer en train de gagner
+                    $(".board").addClass("computer-won");
+                    $(".board").children().remove();
+                    //sound off
+                    $("audio").remove()
+                }
+            }
+        },1000);
+
     });
 });
